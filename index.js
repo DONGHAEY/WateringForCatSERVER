127bbfacfa2a8c00b8bb976ed0707a537e39892a
DONGHAEY <dongheaey@gmail.com>
2021-08-04 14:58:06 +0900

    bcrypt를 사용하여 비밀번호를 암호화

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express =require("express");
-const app = express();
-const port = 3000;
+const express =require("express"); //express모듈을 불러온다
+const app = express(); //app에다가 express를 사용할 수 있도록 해준다
+const port = 3000; //port를 설정한다
 
 const { User } = require("./models/User"); //User.js에서 내보내는 User모델을 불러온다.
-const mongoose = require("mongoose"); //몽구스 모듈을 불러왔다..
+
+const mongoose = require("mongoose"); //몽고DB를 사용하기 위해 몽구스 모듈을 불러왔다..
 
 const config = require("./config/key"); //keyjs를 실행하여 불러와
 const keyValue = config.mongoURI; //mongourl를 가져온다
@@ -18,12 +19,15 @@ mongoose.connect(keyValue, {
 
 app.get ("/", (req, res) => { //'/'로 접속하면 helloworld를 respond하라
     res.send("Hello World!");
-    console.log(config.mongoURI);
 });
 
-app.post('/register', (req, res)=> {
-    const user = new User(req.body);
-    console.log (user);
+app.post('/posts', (req, res) => {
+    const HwData = req.body;
+    console.log(HwData);
+});
+
+app.post('/register', (req, res)=> { // /register로 POST요청이 들어오면
+    const user = new User(req.body); //user는 post로 받아온값을 User()통해 DB에 생성한다
     user.save((err, userInfo) => {
         if(err) return res.json ({ success: false, err});
         return res.status(200).json({success: true});
@@ -32,4 +36,4 @@ app.post('/register', (req, res)=> {
 //회원 가입 할 때, 필요한 정보들을 client에서 가져오면
 //그것들을 데이터 베이스에 넣어준다.
 
-app.listen(port, () => console.log(`started on ${port}!`));
+app.listen(port, () => console.log(`started on ${port}!`)); //app.listen을 사용하여 서버를 실행시킨다, 그리고 몇번 포트에서 열렸는지 알려주는 함수를 실행한다.
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name : {
@@ -32,8 +34,27 @@ const userSchema = mongoose.Schema({
     Hwkey : {
         type:String
     }
+});
+
+userSchema.pre('save', function(next) { //몽구스에서 save logic 실행전에 이 함수를 실행하라
+    //비밀번호를 암호화하는 함수 작성
+    const user = this; //userSchema를 말한다
+    if (user.isModified('password')) { //password가 변환되었을때 아래 로직을 수행한다
+        bcrypt.genSalt(saltRounds, (err, salt) => { //genSalt를 사용하여 saltRounds를 보내고, err와 salt를 받아 함수를 실행한다.
+            if(err) return next(err); //에러가 있다면 err를 주고 다음으로 넘어가라
+            bcrypt.hash(user.password, salt, (err, hash) => { //암호화해주는 핵심 부분 //hash를 통해 비밀번호를 보내고, 위의 함수에서 받은 salt를 보내고, err와 hash를 받아서 아래 로직을 실행하라. 
+                if(err) return next(err); //만약 에러가 있따면 err을 주고 넘어가라
+                user.password = hash; //암호화 된것으로 바꿔준다
+                console.log(hash);
+                next(); //다음으로 넘어가라
+            })
+        })
+    }
+    else {
+        next(); //비밀번호를 바꾼는게 아니면 그냥 넘어가라..
+    }
 })
 
-const User = mongoose.model('User', userSchema) //User라는 이름으로 모델을 만들고 그 모델안에는 userSchema object를 스키마로 넣어라..
+const User = mongoose.model('User', userSchema) //User라는 이름으로 모델(콜렉션)을 만들고 그 모델안에는 userSchema object를 스키마로 넣어라..
 
-module.exports = { User };
+module.exports = { User }; // 내보낸다.
